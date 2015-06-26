e89f306aa604efc8e52ece99783c1404e3d5ec5d
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2015-06-26 10:03:29 +0200

    fix client side implementation

--- src/main/webapp/search-request.js
+++ src/main/webapp/search-request.js
@@ -1,78 +1,59 @@
 // To be implemented by salah 
-function loadSearchImageAsBinary(inputControl){
-var can = document.getElementById('canvas1'); 
-var ctx = can.getContext('2d'); 
-var img = new Image(); 
-img.src = inputControl; 
-img.onload = function ()
-{ can.width = img.width; 
-can.height = img.height; 
-ctx.drawImage(img, 0, 0, img.width, img.height); 
-} 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', inputControl, true);
-xhr.responseType = 'arraybuffer';
+//function loadSearchImageAsBinary(inputControl){
+//	var xhr = new XMLHttpRequest();
+//	xhr.open('GET', inputControl, true);
+//	xhr.responseType = 'arraybuffer';
+//	
+//	xhr.onload = function(e) {
+//	  
+//	  var responseArray = new Uint8Array(this.response); 
+//	};
+//	
+//	xhr.send();
+////ctx.drawImage(img, 0, 0, img.width, img.height); 
+// return responseArray; 
+//}
 
-xhr.onload = function(e) {
-  
-  var responseArray = new Uint8Array(this.response); 
-};
+// To be implemented by salah
 
-xhr.send();
-//ctx.drawImage(img, 0, 0, img.width, img.height); 
- return responseArray; 
-}
-
-// To be implemented by salah 
-function loadSearchImageAsDataURI(inputControl){
-var can = document.getElementById('canvas');
-        var ctx = can.getContext('2d');
-         
-        var img = new Image();
-		img.src = inputControl;
-        img.onload = function (){
-			
-            can.width = img.width;
-            can.height = img.height;
-            ctx.drawImage(img, 0, 0, img.width, img.height);
-		  
-};
-       var dataURL = canvas.toDataURL(inputControl);
-       //alert( dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-	   return dataURL;
+function loadSearchImageAsDataURI(inputControl, outputImage, callback){
+	if(FileReader){
+		var fr = new FileReader();
+		fr.onload = function(){
+			outputImage.src = fr.result;
+			callback(outputImage);
+		};
+		fr.readAsDataURL(inputControl.files[0]);
+	}
 }
 
 // To be implemented by kamaly 
-function doSearchRequest(){
+function doSearchRequest(imageInputControl, callback){
 	// return dummy data to be used in implementation
 	// extractedImageUrl should be added after splitting images, it is put here so that search-results work don't wait for image-split work
-	return {
+	callback({
 		'foundObjects': [{
 			'type': 'bike',
-			'bounds': {
-				'x': 0,
-				'y': 0,
-				'width': 10,
-				'height': 10
-			}, 
+			'x': 0,
+			'y': 0,
+			'width': 300,
+			'height': 400, 
 			'extractedImageUrl': 'dummyImages/bikeExtracted.jpg',
 			'similarImagesUrls': [
-				"dummyImages/bike1.jpg",
-				"dummyImages/bike2.jpg"
+				"dummyImages/bike1.png",
+				"dummyImages/bike2.png"
 			]
 		}, {
 			'type': 'apple',
-			'bounds': {
-				'x': 40,
-				'y': 40,
-				'width': 100,
-				'height': 200
-			}, 
+			'x': 600,
+			'y': 600,
+			'width': 500,
+			'height': 500,
 			'extractedImageUrl': 'dummyImages/appleExtracted.jpg',
 			'similarImagesUrls': [
-				"dummyImages/apple1.jpg",
-				"dummyImages/apple2.jpg"
+				"dummyImages/apple1.png",
+				"dummyImages/apple2.png"
 			]
 		}]
-	};
+	});
 }
--- src/main/webapp/search-results.js
+++ src/main/webapp/search-results.js
@@ -1,7 +1,13 @@
- 
+function clearResults(){
+	var results = document.getElementById('results');
+	results.innerHTML = '';
+}
+
 function showResults(results){
+	clearResults();
+	
 	var foundObjects = results.foundObjects;
-	document.getElementById('results');
+	var results = document.getElementById('results');
 
 	for(var i=0; i<foundObjects.length; i++){
 		var object = foundObjects[i];
@@ -10,28 +16,26 @@ function showResults(results){
 		var extractedImageUrl = object.extractedImageUrl;
 		var similarImagesUrls  = object.similarImagesUrls;
 
-	var showData=document.createElement('div');
-	var typeData=document.createElement('h1');
-	typeData.innerHtml=type;
-	alert("ssas");
-	showData.appendChild(typeData);
-	
-	var extractedimg=document.createElement('img');
-	extractedimg.src=extractedImageUrl;
-	showData.appendChild(extractedimg);
-	alert("ssasasaas");
-	for(var j=0; j<similarImagesUrls.length; j++){
-			var sim_arr=similarImagesUrls[j];
-		alert("aaaa");
-	var similarimg=document.createElement('h2');
-		similarimg.innerHtml=sim_arr;
-		similarimg.src=similarImagesUrls;
-		similarimg.push(sim_arr);
-		showData.appendChild(similarimg);
-	}	
-		alert("cc");
-	results.appendChild(showData);
-	
-	}
+		var showData = document.createElement('div');
 		
+		var typeData = document.createElement('h1');
+		typeData.innerHTML = type;
+		showData.appendChild(typeData);
+		
+		var extractedimg = document.createElement('img');
+		extractedimg.src = extractedImageUrl;
+		showData.appendChild(extractedimg);
+
+		var typeData = document.createElement('h2');
+		typeData.innerHTML = 'Similar images';
+		showData.appendChild(typeData);
+		
+		for(var j=0; j<similarImagesUrls.length; j++){
+			var similarImg = document.createElement('img');
+			similarImg.src = similarImagesUrls[i];
+			showData.appendChild(similarImg);
+		}
+		
+		results.appendChild(showData);	
+	}
 }
